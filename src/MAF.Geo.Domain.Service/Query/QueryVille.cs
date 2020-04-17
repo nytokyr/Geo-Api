cf425d2c279b07c497c5a0b419d7a2d6951d1235
toky <nytokyr@gmail.com>
2020-04-17 15:35:51 +0200

    add queryville

---
+++ src/MAF.Geo.Domain.Service/Query/QueryVille.cs
@@ -0,0 +1,18 @@
+﻿namespace MAF.Geo.Domain.Service
+{
+    public class QueryVille
+    {
+        public int PaysId { get; internal set; }
+        public string CodePostal { get; internal set; }
+        public string CodeInsee { get; internal set; }
+        public string AutoComplete { get; internal set; }
+
+        private QueryVille() { }
+
+        public static QueryVille Create(int paysId)
+        {
+            return new QueryVille() { PaysId = paysId };
+        }
+    }
+}
+
---
+++ src/MAF.Geo.Domain.Service/Query/QueryVilleExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace MAF.Geo.Domain.Service
+{
+    public static class QueryVilleExtensions
+    {
+        public static QueryVille WithCodePostal(this QueryVille query, string codePostal)
+        {
+            query.CodePostal = codePostal;
+            return query;
+        }
+
+        public static QueryVille WithCodeInsee(this QueryVille query, string codeInsee)
+        {
+            query.CodeInsee = codeInsee;
+            return query;
+        }
+
+        public static QueryVille WithAutoComplete(this QueryVille query, string autocomplete)
+        {
+            query.AutoComplete = autocomplete;
+            return query;
+        }
+    }
+}
